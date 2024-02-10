2b4008a0659eb9553cc04adc555e16951dbd12bc
RafaelSSantiago <raphasantiago768@gmail.com>
2024-02-10 09:45:19 -0300

    fix: remove coverage past

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {

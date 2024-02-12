1aec770ffad1b3aacdc5009dcb028e32e48e57e2
RafaelSSantiago <raphasantiago768@gmail.com>
2024-02-12 17:40:15 -0300

    refactor: values

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import { BcrypterAdapter } from './bcrypt-adapter' 
 
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct value', async () => {
+  test('Should call bcrypt with correct values', async () => {
     const salt = 12
     const sut = new BcrypterAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
